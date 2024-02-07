aad192486c84e290b724608395180621c06d60ff
Salva91 <salvatore_giordano1991@live.it>
2024-02-07 20:30:47 +0100

    esercizo completato

--- m1/s3/g3/javascript/file.js
+++ m1/s3/g3/javascript/file.js
@@ -1,2 +1,27 @@
+const bottone = document.querySelector("#bottone");
+const input = document.querySelector("#taskInput");
+const lista = document.querySelector("#lista");
+
+bottone.addEventListener('click', function() {
+    let li = document.createElement('li');
+    li.innerText = input.value;
+    
+
+    li.addEventListener('click', () => {
+        li.style.setProperty("text-decoration", "line-through");
+    });
+    let btn = document.createElement('button');
+    btn.innerText = "elimina";
+
+    btn.addEventListener('click',function(){
+        li.remove()
+    })
+
+    li.append(btn)
+    lista.append(li);
+});
+
+
+
 
 
