2278bc44db4ccf8fafee963f0ac1fac6905be562
Salva91 <salvatore_giordano1991@live.it>
2024-02-08 16:28:39 +0100

    esercizio giorno 4 tombola

---
+++ m1/s3/g4/js/file.js
@@ -0,0 +1,37 @@
+let tabellone = document.querySelector('.tabellone');
+
+for(let i = 1; i <= 90; i++ ){
+    let numeri = document.createElement('div');
+    numeri.classList.add('celle');
+    numeri.innerText = i;
+    tabellone.append(numeri);
+}
+
+let creaBottone = document.querySelector('.conteiner-bottone');
+let bottone = document.createElement('button');
+bottone.innerText = 'premi qui';
+creaBottone.appendChild(bottone);
+
+let risultatoElement = document.createElement('p');
+creaBottone.appendChild(risultatoElement);
+
+let numeriGenerati = [];
+
+bottone.addEventListener('click', function() {
+    let numeroCasuale;
+      
+    do {
+        numeroCasuale = Math.floor(Math.random() * 90) + 1;
+    } while (numeriGenerati.includes(numeroCasuale));
+
+    numeriGenerati.push(numeroCasuale);
+
+    risultatoElement.innerText = "Numero generato: " + numeroCasuale;
+
+    let numeriTabellone = document.querySelectorAll('.celle');
+    numeriTabellone.forEach(function(numero) {
+        if (numero.innerText == numeroCasuale) {
+            numero.classList.add('selezionato');
+        }
+    });
+});
