25ea2c4078417db795fe3af0c7bba13a4298d683
Salva91 <salvatore_giordano1991@live.it>
2024-04-15 18:54:58 +0200

    esercizio aggiunto

--- m1-java/settimana1/g1/esercitazione1/src/Esercizio2.java
+++ m1-java/settimana1/g1/esercitazione1/src/Esercizio2.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Esercizio2 {
+import java.util.Scanner;
+
+public class Esercizio2 {
+    public static void main(String[] args) {
+        String variabile1 = input("1° stringa: ");
+        String variabile2 = input("2° stringa: ");
+        String variabile3 = input("3° stringa: ");
+
+        String concatenata = variabile1 + variabile2 + variabile3;
+        System.out.println("Stringa concatenata: " + concatenata);
+    }
+
+    public static String input(String prompt) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(prompt);
+        return sc.nextLine();
+    }
 }
+
+
