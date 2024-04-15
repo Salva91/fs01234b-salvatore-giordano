c8583ac1457795c5f4e47f819cc99a59f44bc7ff
Salva91 <salvatore_giordano1991@live.it>
2024-04-15 19:06:02 +0200

    aggiunta pari o dispari con input

--- m1-java/settimana1/g1/esercitazione1/src/Esercizio3.java
+++ m1-java/settimana1/g1/esercitazione1/src/Esercizio3.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Esercizio3 {
+import java.util.Scanner;
+
+
+public class Esercizio3 {
+
+    public static void main(String[] args) {
+        int numero = input("Inserisci un numero: ");
+        pariDispari(numero);
+    }
+
+    public static int input(String prompt) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(prompt);
+        return sc.nextInt();
+    }
+
+    public static void pariDispari(int numero) {
+        if (numero % 2 == 0) {
+            System.out.println("Il numero è pari");
+        } else {
+            System.out.println("Il numero è dispari");
+        }
+    }
 }
