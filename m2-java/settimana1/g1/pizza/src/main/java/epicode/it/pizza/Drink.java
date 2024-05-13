d33bdb5303acfc7b9249af41a4d4290e1db163b2
Salva91 <salvatore_giordano1991@live.it>
2024-05-13 16:34:17 +0200

    esercitazione spring boot pizza

---
+++ m2-java/settimana1/g1/pizza/src/main/java/epicode/it/pizza/Drink.java
@@ -0,0 +1,25 @@
+package epicode.it.pizza;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Drink extends Item {
+	private String name;
+
+	public Drink(String name, int calories, double price) {
+		super(calories, price);
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Drink{" +
+				"name='" + name + '\'' +
+				", calories=" + calories +
+				", price=" + price +
+				'}';
+	}
+}
+
